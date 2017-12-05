0d1bf9bede362a8587a2b313205dc7c08c676839
Sandesh Soni <hello@SandeshSoni.com>
2017-12-05 20:09:41 +0530

    completed second part of day one

--- one.ex
+++ one.ex
@@ -7,7 +7,26 @@ defmodule One do
   end
 
   def calculate_two(input) do
-    digits_list(input)
+    list = digits_list(input)
+    half = div(Enum.count(list), 2)
+    l_w_i = Enum.with_index(Enum.take list, half)
+    Enum.reduce(l_w_i, 0,
+    fn({k,i}, acc) ->
+      # IO.puts "----"
+      #IO.puts "acc ==>> #{ acc }"
+      #IO.puts "case =>> #{Enum.at(list, i + half)}"
+      #IO.puts "k ==>> #{ k }"
+      #IO.puts "acc + k + k =>> #{(acc + k + k)}"
+      case Enum.at(list,i+half) do
+        ^k  ->
+          # IO.puts "match"
+          (acc + k + k)
+        _ ->
+          # IO.puts "NOT match"
+          acc
+      end
+    end
+    )
   end
 
   def circular_digits_list(str) do
